eff9aa2cd6e922943e013806098704299506e5a5
Yash Rai <yash.rai@wehealapp.in>
2024-06-27 14:55:55 +0530

    first commit

---
+++ components/about.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        I graduated with a B.Tech in{" "}
+        <span className="font-medium">Computer Science & Engineering</span> from
+        SR Group of Institutions, Jhansi. With a strong foundation in computer
+        science, I have specialized in{" "}
+        <span className="font-medium">Data Engineering</span>. My passion lies
+        in building scalable data pipelines and working with{" "}
+        <span className="font-medium">big data technologies</span>.
+      </p>
+
+      <p className="mb-3">
+        Professionally, I have worked as a{" "}
+        <span className="font-medium">Software Development Engineer Intern</span>{" "}
+        at Clarity, where I developed a Customer Data Platform (CDP) web application
+        that significantly improved user data management and fraud detection.
+        I engineered a real-time big data pipeline using{" "}
+        <span className="font-medium">Python and Apache Beam</span>, processing
+        over 5 million user events daily, and implemented an ETL pipeline to
+        reduce data handling costs.
+      </p>
+
+      <p className="mb-3">
+        My technical proficiencies include{" "}
+        <span className="font-medium">Python, SQL, and big data tools</span>{" "}
+        such as Apache Beam, Pub/Sub, and Databricks. I am also skilled in
+        cloud services, particularly <span className="font-medium">Google Cloud Platform (GCP)</span>, and database systems like BigQuery and Bigtable. My experience spans across
+        <span className="font-medium"> developing data solutions</span> that optimize workflows and enhance system performance.
+      </p>
+
+      <p className="mb-3">
+        In addition to my professional expertise, I enjoy exploring new
+        technologies and continuously expanding my knowledge. Currently, I am
+        looking for opportunities to apply my skills in a{" "}
+        <span className="font-medium">Data Engineer role</span> where I can
+        contribute to innovative projects and solve complex data problems.
+      </p>
+
+      <p>
+        <span className="italic">Outside of work</span>, I have a keen interest
+        in playing video games, watching movies, and diving into{" "}
+        <span className="font-medium">technical challenges</span> as a hobby. 
+        I am also enthusiastic about learning new skills and recently, I've been exploring{" "}
+        <span className="font-medium">cloud technologies and data architecture</span>.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,111 @@
+import React from "react";
+import { IoLogoHtml5 } from "react-icons/io5";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import { FaDatabase } from "react-icons/fa6";
+import etl from "@/public/etl.png";
+import dataform from "@/public/dataform.webp";
+import url from "@/public/url.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Software Development Engineer Intern",
+    location: "Clarity",
+    description:
+      "As a Software Development Engineer Intern at Clarity from February 2024 to June 2024, I spearheaded the development of a Customer Data Platform (CDP) web application. This application revolutionized user data management by enhancing the detection of events, payments, and fraudulent activities. Utilizing Python and Apache Beam, I engineered a real-time big data pipeline capable of processing over 5 million user events daily from Pub/Sub to BigQuery and Bigtable. Additionally, I implemented an ETL pipeline that streamlined real-time data processing from Pub/Sub, leveraging Apache Beam to transform and store data in BigQuery, resulting in a 10% reduction in data handling costs.",
+    icon: React.createElement(FaDatabase),
+    date: "Feb 2024 – Jun 2024",
+  },
+  {
+    title: "UI/UX Developer Intern",
+    location: "BinPlus Technologies",
+    description:
+      "At BinPlus Technologies from November 2023 to January 2024, I played a pivotal role as a UI/UX Developer Intern. I contributed to a casino game project by integrating Socket.IO components, enabling seamless real-time gameplay connectivity. Leveraging React, I developed interactive components that enhanced online gameplay interactions. Furthermore, I designed and developed a betting website from scratch, integrating various APIs to support dynamic user management functionalities. I also implemented robust authentication and authorization mechanisms using Node.js, ensuring secure API endpoints with token-based authentication.",
+    icon: React.createElement(FaReact),
+    date: "Nov 2023 – Jan 2024",
+  },
+  {
+    title: "Web Developer Intern",
+    location: "Abhyaz",
+    description:
+      "During my tenure as a Web Developer Intern at Abhyaz from December 2022 to May 2023, I focused on maintaining and developing dynamic and responsive web applications on the Zoho platform. I successfully managed and enhanced three different websites, utilizing Zoho Sites to implement features such as forms, calendar event markers, and various interactive elements. My contributions significantly improved user experience across these platforms, demonstrating my proficiency in web development and UI/UX design principles.",
+    icon: React.createElement(IoLogoHtml5),
+    date: "Dec 2022 – May 2023",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Real-Time Streaming Data Pipeline",
+    description:
+      "Designed and implemented a robust real-time streaming data pipeline using Python, Apache Beam, Google Cloud Pub/Sub, Apache Kafka, BigQuery, and Bigtable. The pipeline was engineered to handle a dynamic range of 20-25 topics sourced from Apache Kafka via Google Pub/Sub, ensuring efficient data ingestion and processing. Utilizing Apache Beam (Python), I developed data transformation and cleansing processes to ensure data quality and consistency before storage. The entire pipeline was deployed and managed on Google Dataflow, enabling seamless real-time data processing and monitoring.",
+    tags: ["Python", "Apache Beam", "Google Cloud", "BigQuery", "Bigtable", "Real-Time Data", "Data Engineering"],
+    imageUrl: etl,
+  },
+  {
+    title: "Raw Data to Incremental Table",
+    description:
+      "Engineered and implemented a data pipeline using Google Dataform and BigQuery to transform raw data into incremental views for comprehensive analytics. Leveraging Google Dataform, I orchestrated the extraction and transformation of raw data from diverse sources, ensuring alignment with business requirements. By creating and joining multiple views, I facilitated detailed data analysis and reporting. Automation of the pipeline enabled scheduled runs, ensuring timely availability of updated data for strategic decision-making.",
+    tags: ["Google Dataform", "BigQuery", "Data Transformation", "Data Analytics", "Automation"],
+    imageUrl: dataform,
+  },
+  {
+    title: "URL Shortener",
+    description:
+      "Developed a feature-rich URL shortener service using Node.js, Firebase, ReactJS, and Tailwind CSS. Implemented robust user authentication and authorization mechanisms via Firebase, ensuring secure access to shortened URLs. Leveraging Firebase database, I designed a scalable architecture for storing and managing shortened URLs with efficient validation and retrieval mechanisms. The user interface, built with ReactJS and Tailwind CSS, prioritized usability and aesthetics, offering a seamless experience for users to generate and manage shortened URLs effectively.",
+    tags: ["Node.js", "Firebase", "ReactJS", "Tailwind CSS", "URL Shortener", "Authentication", "Database Management"],
+    imageUrl: url,
+  },
+] as const;
+
+
+export const skillsData = [
+  "Python",
+  "SQL",
+  "Apache Beam",
+  "Google Cloud Platform (GCP)",
+  "BigQuery",
+  "Bigtable",
+  "Pub/Sub",
+  "Databricks",
+  "Spark",
+  "Kafka",
+  "ETL",
+  "Data Pipelines",
+  "Data Warehousing",
+  "Data Engineering",
+  "Cloud Computing",
+  "Database Management",
+  "Git",
+  "Docker",
+  "Kubernetes",
+  "REST APIs",
+] as const;
+
