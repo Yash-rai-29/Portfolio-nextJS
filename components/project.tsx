d3c6751b2bb38a6ba2aa3ae7050907b1747dfd15
Yash Rai <yash.rai@wehealapp.in>
2024-07-09 15:18:17 +0530

    fix project aligment and width

--- components/project.tsx
+++ components/project.tsx
@@ -31,14 +31,14 @@ export default function Project({
       className="group mb-6 sm:mb-12 last:mb-0"
     >
       <section
-        className="bg-gray-100 max-w-[40rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[24rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20"
+        className="bg-gray-100 max-w-[46rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[24rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20"
       >
-        <div className="pt-6 pb-8 px-6 sm:pl-10 sm:pr-6 sm:pt-12 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[20rem] overflow-hidden relative">
+        <div className="pt-6 pb-8 px-6 sm:pl-10 sm:pr-1 sm:pt-12 sm:max-w-[68%] flex flex-col h-full sm:group-even:ml-[14rem] overflow-hidden relative">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <ul className="flex flex-wrap gap-2 mt-2 mb-4">
             {tags.map((tag, index) => (
               <li
-                className="bg-black/[0.6] px-2 py-1 text-[0.4rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                className="bg-black/[0.6] px-2 py-1 text-[0.5rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
                 key={index}
               >
                 {tag}
