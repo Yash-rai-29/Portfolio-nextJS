8b403b040b3ee45fcbe1b2db640062a1b2eda0af
Yash Rai <yash.rai@wehealapp.in>
2024-08-07 14:31:44 +0530

    fix contact form error

--- components/contact.tsx
+++ components/contact.tsx
@@ -34,37 +34,35 @@ export default function Contact() {
         </p>
 
         <form
-          className="mt-10 flex flex-col dark:text-black"
-          onSubmit={async (e) => {
-            e.preventDefault();
-            const formData = new FormData(e.target);
-            const { data, error } = await sendEmail(formData);
+        className="mt-10 flex flex-col dark:text-black"
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
 
-            if (error) {
-              toast.error(error);
-              return;
-            }
+          if (error) {
+            toast.error(error);
+            return;
+          }
 
-            toast.success("Email sent successfully!");
-          }}
-        >
-          <input
-            className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-            name="senderEmail"
-            type="email"
-            required
-            maxLength={500}
-            placeholder="Your email"
-          />
-          <textarea
-            className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
-            name="message"
-            placeholder="Your message"
-            required
-            maxLength={5000}
-          />
-          <SubmitBtn />
-        </form>
+          toast.success("Email sent successfully!");
+        }}
+      >
+        <input
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        />
+        <SubmitBtn />
+      </form>
       </motion.section>
       <Footer />
     </>
